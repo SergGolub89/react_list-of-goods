2245e4639b22e00ff56595d1bca343cff4d8d057
SerGolub89 <k3210389gsi@gmail.com>
2019-07-20 13:31:13 +0300

    Updates

---
+++ precache-manifest.322cdb21c6f21a050732ecee09042890.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1749a794131b5f1745369f23a5d73b",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/react_list-of-goods/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "434e0a621808dcfc3be8",
+    "url": "/react_list-of-goods/static/js/main.8a0c30cf.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
